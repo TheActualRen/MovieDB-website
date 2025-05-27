729c2b5e81b2c4567fe07af4fb31bf575e332b7a
TheActualRen <mandaliadhiren@gmail.com>
2025-05-27 22:14:26 +0100

    Hopefully set up models.py correctly :)

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,7 +1,9 @@
 from django.db import models
-
+from django.db.models.deletion import CASCADE
 
 # User Table
+
+
 class UserTable(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
@@ -15,6 +17,8 @@ class UserTable(models.Model):
 
 
 # Main Tables
+
+
 class MoviesTable(models.Model):
     movie_name = models.CharField(max_length=255)
     release_year = models.IntegerField()
@@ -101,3 +105,56 @@ class RatingsTable(models.Model):
 
     def __str__(self):
         return f"{self.rating_value} | {self.user_id}"
+
+
+# Junction Tables
+
+
+class MovieDirectorsTable(models.Model):
+    movie = models.ForeignKey(
+        MoviesTable, on_delete=models.CASCADE, db_column="movie_id"
+    )
+    director = models.ForeignKey(
+        DirectorsTable, on_delete=CASCADE, db_column="director_id"
+    )
+
+    class Meta:
+        unique_together = ("movie", "director")
+        db_table = "Movie_Directors"
+
+
+class MovieWritersTable(models.Model):
+    movie = models.ForeignKey(
+        MoviesTable, on_delete=models.CASCADE, db_column="movie_id"
+    )
+    writer = models.ForeignKey(WritersTable, on_delete=CASCADE, db_column="writer_id")
+
+    class Meta:
+        unique_together = ("movie", "writer")
+        db_table = "Movie_Writers"
+
+
+class MovieActorsTable(models.Model):
+    movie = models.ForeignKey(
+        MoviesTable, on_delete=models.CASCADE, db_column="movie_id"
+    )
+    actor = models.ForeignKey(
+        ActorsTable, on_delete=models.CASCADE, db_column="actor_id"
+    )
+
+    class Meta:
+        unique_together = ("movie", "actor")
+        db_table = "Movie_Actors"
+
+
+class MovieGenres(models.Model):
+    movie = models.ForeignKey(
+        MoviesTable, on_delete=models.CASCADE, db_column="movie_id"
+    )
+    genre = models.ForeignKey(
+        GenresTable, on_delete=models.CASCADE, db_column="genre_id"
+    )
+
+    class Meta:
+        unique_together = ("movie", "genre")
+        db_table = "Movie_Genres"
